b91d3a93d8c23a594eec2120800fcebb14556747
Laxmikant Suryavanshi <laxmikantsuryavanshi@hotmail.com>
2020-08-05 18:10:57 +0530

    seperated trace and ignore in, out

--- local.cpp
+++ local.cpp
@@ -1,30 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 #ifdef LOCAL_DEBUG
-template <typename T> ostream&
-operator<<(ostream& os, vector<T> V)
-{ os << "[ "; for(auto v: V) os << v << " ";
-  os << "]"; return os; /*prints std::vector*/ }
-template <typename T, typename U> ostream&
-operator<<(ostream& os, pair<T, U> P)
-{ os << "(" << P.first << ", " << P.second;
-  return os << ")"; /*prints std::pair*/ }
-template <typename T, typename U> ostream&
-operator<<(ostream& os, map<T, U> V)
-{ os << "{ "; for(auto v: V) os << v << " ";
-  os << "}"; return os; /*prints std::map*/ }
-#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
-template <typename Arg> void
-__f(const char* name, Arg&& arg)
-{ cout << name << ": " << arg << '\n'; }
-template <typename Arg1, typename... Args> void
-__f(const char* names, Arg1&& arg1, Args&&... args)
-{ const char* comma = strchr(names+1, ',');
-  cout.write(names, comma-names) << ": "
-  << arg1 << "| "; __f(comma+1, args...); }
+    #include "/home/laxsuryavanshi/Desktop/cp/trace.hpp"
 #else
-#define trace(...)
+    #define trace(...)
 #endif // LOCAL_DEBUG
+
 void solve_test();
 
 int32_t main() {
---
+++ trace.hpp
@@ -0,0 +1,46 @@
+#ifndef CP_TRACE_HPP
+#define CP_TRACE_HPP
+
+template <typename T>
+ostream&
+operator<<(ostream& os, vector<T> V) {
+    os << "[ ";
+    for(auto v: V) os << v << " ";
+    os << "]"; return os;
+    /*prints std::vector*/
+}
+
+template <typename T, typename U>
+ostream&
+operator<<(ostream& os, pair<T, U> P) {
+    os << "(" << P.first << ", " << P.second;
+    return os << ")";
+    /*prints std::pair*/
+}
+
+template <typename T, typename U>
+ostream&
+operator<<(ostream& os, map<T, U> V) {
+    os << "{ ";
+    for(auto v: V) os << v << " ";
+    os << "}"; return os;
+    /*prints std::map*/
+}
+
+#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
+
+template <typename Arg>
+void
+__f(const char* name, Arg&& arg) {
+    cout << name << ": " << arg << '\n';
+}
+
+template <typename Arg1, typename... Args>
+void
+__f(const char* names, Arg1&& arg1, Args&&... args) {
+    const char* comma = strchr(names+1, ',');
+    cout.write(names, comma-names) << ": "
+        << arg1 << "| "; __f(comma+1, args...);
+}
+
+#endif // CP_TRACE_HPP
